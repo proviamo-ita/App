0afd776c041dd38dc53f9a2cda14de00dbf091b8
alessandroquirile <alessandroquirile@alessandroquirile.homenet.telecomitalia.it>
2019-11-12 13:47:41 +0100

    Added a new Saluto.

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,6 +5,8 @@
  */
 package main;
 
+import classes.Saluto;
+
 /**
  *
  * @author Alessandro Quirile
@@ -17,6 +19,11 @@ public class Main {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Ciao");
+        
+        Saluto saluto = new Saluto();
+        
+        saluto.stampa("Sono un nuovo saluto");
+        
     }
     
 }
