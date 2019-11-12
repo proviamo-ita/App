8832b971f71c7d4ae4ed47658b0214725cf229f2
alessandroquirile <alessandroquirile@alessandroquirile.homenet.telecomitalia.it>
2019-11-12 13:45:03 +0100

    Deleted two lines.

--- src/main/Main.java
+++ src/main/Main.java
@@ -17,8 +17,6 @@ public class Main {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Ciao");
-        System.out.println("Aggiunta nuova linea");
-        System.out.println("Aggiunta una terza linea");
     }
     
 }
