c180f0d738d8dbd454ecfc53b33042f7c60cd811
alessandroquirile <alessandroquirile@alessandroquirile.homenet.telecomitalia.it>
2019-11-12 13:40:45 +0100

    Add new line.

--- src/main/Main.java
+++ src/main/Main.java
@@ -17,6 +17,7 @@ public class Main {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Ciao");
+        System.out.println("Aggiunta nuova linea");
     }
     
 }
